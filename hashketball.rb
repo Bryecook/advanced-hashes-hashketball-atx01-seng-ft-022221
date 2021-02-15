7ab438d81a19124b0feb6dbbe8408a4527dd4cf8
Brian Cook <brybcook@gmail.com>
2021-02-15 17:07:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,10 +155,9 @@ def team_colors(team)
     end
 end
 
-def team_names(name)
+def team_names
   game_hash.each do |location, team_data|
-    if team_data[:team_name]==name
-      return team_data[:team_name]
+    return team_data[:team_name]
     end
   end
 end
